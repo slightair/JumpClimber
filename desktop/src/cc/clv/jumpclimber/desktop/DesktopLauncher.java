8f19e94ddd686141f1ee155c164806c17489a69c
slightair <arksutite@gmail.com>
2015-09-21 12:38:11 +0900

    Generate LibGDX project

---
+++ desktop/src/cc/clv/jumpclimber/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cc.clv.jumpclimber.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cc.clv.jumpclimber.JumpClimber;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JumpClimber(), config);
+	}
+}
